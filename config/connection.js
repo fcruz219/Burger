2c0291465e3ec2dd4d7e7b20034e26e9c080a743
Francisco <fcruz219@gmail.com>
2019-04-06 16:19:53 -0400

    main page and index are not showing up on localhost. server.js/connection.js/ are done.

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  // DB is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
     password: "root",
     database: "burgers_db"
   });
-  
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
-  });
+}
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
 
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
 
-var app = express();
-
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +18,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgercontroller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
